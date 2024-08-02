b7440c1ac9164f30174385454c035a658b450bbb
UCHINO Yuki <160313760+UCHINO-Yuki@users.noreply.github.com>
2024-08-02 17:29:33 +0900

    Add files via upload

---
+++ job256.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#PJM -L "node=256"               # num. of nodes
+#PJM -L "rscgrp=small-torus"     # int:1-12, small:1-384, large:385-55296
+#PJM -L "elapse=05:00:00"        # time limit
+#PJM -g PROJECT                  # group of project
+#PJM --mpi "max-proc-per-node=4" # max num. of MPI process per node
+#PJM -s
+
+export PLE_MPI_STD_EMPTYFILE=off 
+export OMP_NUM_THREADS=12 
+export LD_LIBRARY_PATH=../src:../EigenExa-2.12/lib:$LD_LIBRARY_PATH
+
+mpiexec ./test_refsyevcl 14 5 3
+mpiexec ./test_refsyevcl 14 10 3
+mpiexec ./test_refsyevcl 15 5 3
+mpiexec ./test_refsyevcl 15 10 3
+mpiexec ./test_refsyevcl 16 5 3
+mpiexec ./test_refsyevcl 16 10 3
+mpiexec ./test_refsyevcl 17 5 3
+mpiexec ./test_refsyevcl 17 10 3
+
---
+++ submit.sh
@@ -0,0 +1,5 @@
+pjsub --no-check-gfscache job64.sh
+pjsub --no-check-gfscache job128.sh
+pjsub --no-check-gfscache job256.sh
+pjsub --no-check-gfscache job512.sh
+pjsub --no-check-gfscache job1024.sh
